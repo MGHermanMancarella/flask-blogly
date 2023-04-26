3a7c305d2e8da351ba7330d65445552c2a942444
Steven <stzheng716@gmail.com>
2023-04-26 12:41:23 -0700

    add base and index html

--- app.py
+++ app.py
@@ -2,13 +2,19 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    "DATABASE_URL", 'postgresql:///blogly')
+    "DATABASE_URL", 'postgresql:///blogly') #TODO: change db url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+
+@app.get('/')
+def homepage():
+    
+    return render_template("index.html")
 
 connect_db(app)
