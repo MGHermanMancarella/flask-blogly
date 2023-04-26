2d8317e333196fa9881fd65800974bb39c430184
Michael <mherman315@gmail.com>
2023-04-26 15:59:10 -0700

    html set

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ from models import connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    "DATABASE_URL", 'postgresql:///blogly') #TODO: change db url
+    "DATABASE_URL", 'postgresql:///blogly')  # TODO: change db url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+
 @app.get('/')
 def homepage():
-    
-    return render_template("index.html")
+
+    return render_template("users.html")
+
 
 connect_db(app)
